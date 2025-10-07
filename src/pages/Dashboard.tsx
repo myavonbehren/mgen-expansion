dbf10359f42a96e51e02b946f78f4dfa5ba7ecbc
Mya Von Behren <mvonbehr@depaul.edu>
2025-10-07 13:54:25 -0400

    Enhance Dashboard layout by adding new components: ActivityLog, SmartRecs, WeeklyProgress, MyGennieDashboard, and WeeklyReview. Refactor structure for improved responsiveness and organization.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,12 @@ import { useAuth } from '../contexts/AuthContext';
 import SidebardComponent from '../components/sidebar-main';
 import '../styles/Dashboard.css';
 import OverviewStatsDashboard from '../components/overview-stats-dashboard';
+import ActivityLog from '../components/acitivity-log';
+import SmartRecs from '@/components/smart-recs';
+import { WeeklyProgress } from '@/components/weekly-progress';
+import MyGennieDashboard from '@/components/mygennie-dashboard';
+import WeeklyReview from '@/components/weekly-review';
+
 
 const Dashboard = () => {
   const { user, isLoading } = useAuth();
@@ -20,11 +26,29 @@ const Dashboard = () => {
 
   return (
     <SidebardComponent title="Dashboard">
-      <h1 className="text-2xl font-medium">Hi, {user.firstName} {user.lastName.charAt(0).toUpperCase()}!</h1>
-      <h3 className="text-lg font-light text-muted-foreground mb-6">Ready to continue your wellness journey?</h3>
-
-      <OverviewStatsDashboard />
-
+      <div className="flex flex-col"> 
+        <h1 className="text-2xl font-medium">Hi, {user.firstName} {user.lastName.charAt(0).toUpperCase()}!</h1>
+        <h3 className="text-lg font-light text-muted-foreground">Ready to continue your wellness journey?</h3>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4 items-stretch">
+            <div className="h-full min-w-0">
+              <SmartRecs />
+            </div>
+            <div className="h-full min-w-0">
+              <OverviewStatsDashboard/>
+            </div>
+          </div>
+          
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4 items-stretc">
+            <div className="h-full min-w-0">
+                <ActivityLog />
+                <WeeklyProgress />
+            </div>
+            <div className="h-full min-w-0">
+              <WeeklyReview />
+              <MyGennieDashboard />
+            </div>
+          </div>
+      </div>
     </SidebardComponent>
   );
 };
