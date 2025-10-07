7a0760a6a1cbd95553aa4fb4f765554f754da7af
Mya Von Behren <mvonbehr@depaul.edu>
2025-10-07 13:51:36 -0400

    Add DailyProgress component to display daily progress in a card format

---
+++ src/components/daily-progress.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardTitle } from "./ui/card";
+
+const DailyProgress = () => {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Daily Progress</CardTitle>
+            </CardHeader>
+        </Card>
+    )
+}
+
+export default DailyProgress;
